909ccc44dbbd3c4fa7b1ef363a92178f80e6f8a3
LOLandXD <13585566117@139.com>
2026-07-05 12:51:16 +0800

    feat: show attack/transfer preview numbers while dragging; refresh tiles immediately after attack

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_SHELL_CACHE = "new-england-four-app-v52";
-const DATA_CACHE = "new-england-four-data-v52";
+const APP_SHELL_CACHE = "new-england-four-app-v53";
+const DATA_CACHE = "new-england-four-data-v53";
 const APP_SHELL_FILES = [
   "/",
   "/openfreemap_viewer.html",
